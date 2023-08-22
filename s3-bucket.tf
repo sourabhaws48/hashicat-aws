9c877f32bcbaf27cc48bc2e323007d02687a2e6a
Sourabh <sjainaws@amazon.com>
2023-08-22 14:56:54 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,18 +1,16 @@
+
+# s3-bucket.tf
+
 provider "aws" {
   region = "us-east-1"  # Change this to the desired AWS region
 }
 
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "testing5436543343"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
+module "gaurav_s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws" # Update this with the actual path to your module
+  
+  bucket_name = "gaurav-bucket1234312"
+  tags = {
+    Owner = "Gaurav"
+    Environment = "Production"
   }
 }
