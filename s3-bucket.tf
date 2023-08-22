745fb39faa830910fba3096b199ba88dd945cd6f
Sourabh <sjainaws@amazon.com>
2023-08-22 16:24:28 +0000

    Added s3 bucket module2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,11 +3,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/sourabh-test/s3-bucket/aws"
   version = "2.8.0"
-  bucket_name = "gaurav-bucket1234312"
-   acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
+  
 }
 
