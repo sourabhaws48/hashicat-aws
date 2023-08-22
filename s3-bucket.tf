73601d859f86c87b2d7d74b3b7c2c931b3ce167d
Sourabh <sjainaws@amazon.com>
2023-08-22 14:59:22 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -5,8 +5,8 @@ provider "aws" {
   region = "us-east-1"  # Change this to the desired AWS region
 }
 
-module "gaurav_s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws" # Update this with the actual path to your module
+module "gaurav_s3_bucke1212t" {
+  source = "app.terraform.io/sourabh-test/s3-bucket/aws" # Update this with the actual path to your module
   
   bucket_name = "gaurav-bucket1234312"
   tags = {
